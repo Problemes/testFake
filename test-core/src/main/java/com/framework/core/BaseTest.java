e49d6dd43a9b6e45c3542adf085920b320e3567e
Administrator <1191624317@qq.com>
2017-06-07 10:31:06 +0800

    test

---
+++ test-core/src/main/java/com/framework/core/BaseTest.java
@@ -0,0 +1,24 @@
+package com.framework.core;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by HR on 2017/6/7.
+ */
+public class BaseTest {
+
+    @Before
+    public void testBefore(){
+        System.out.println("Testing begin...");
+    }
+
+    //测试反射api
+    @Test
+    public void testReflectApi(){
+
+    }
+
+
+
+}
