82c23712bf41bfd19f4107e40e71a12a89a1821c
Administrator <1191624317@qq.com>
2017-07-28 18:39:02 +0800

    LongTest

--- test-core/src/main/java/com/framework/core/LongTest.java
+++ test-core/src/main/java/com/framework/core/LongTest.java
@@ -1,9 +1,17 @@
 package com.framework.core;
 
+import org.junit.Test;
+
 /**
  * Created by HR on 2017/7/25.
  */
 public class LongTest {
 
-
+    @Test
+    public void testLongEquals(){
+        Long l1 = new Long(100);
+        Long l2 = new Long(100);
+        System.out.println(l1 == l2);
+        System.out.println(l1.equals(l2));
+    }
 }
